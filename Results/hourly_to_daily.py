7bb4fa55e8b4414ee110e2b4052fa6b3aa0674be
Jordan Kern <jkern@ncsu.edu>
2020-11-13 17:14:44 -0500

    update

---
+++ Results/hourly_to_daily.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 27 10:13:47 2020
+
+@author: jkern
+"""
+
+import pandas as pd
+import numpy as np
+
+df_data = pd.read_csv('hourly_hydro.csv',header=0)

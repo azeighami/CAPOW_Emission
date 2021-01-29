e5acef7d36c7ea04afb81a3a4a3504bb882e1786
Jordan Kern <jkern@ncsu.edu>
2021-01-29 12:24:37 -0500

    prob plot update

--- Post Process/Distribution.py
+++ Post Process/Distribution.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 """
 Created on Mon Jan 11 14:08:02 2021
 
@@ -11,44 +12,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+
 def process(data):
 
     means = np.median(data, axis= 0)
-    std=data.std(0)
-    perc=np.zeros((2,365))
-    for i in range(0,365):
-    
-        perc[0,i]=means[i]-np.percentile(data[i,:],25)
-        perc[1,i]=np.percentile(data[i,:],75)-means[i]
-    
     
     ######    # colormap = cm.RdBu_r # change this for the colormap of choice
     colormap = plt.cm.RdBu_r # change this for the colormap of choice
     percentiles = [0,1,5,10,25,50,75,90,95,99,100]
     
-    half = int((len(percentiles)-1)/2)
     L = int(len(percentiles))
     
     SDist=np.zeros((365,L))
     for i in range(L):
         for t in range(365):
-          SDist[t,i]=np.percentile(data[t,:],percentiles[i])
+          SDist[t,i]=np.percentile(data[:,t],percentiles[i])
     SDist[:,5]=means
     
-    return half,percentiles,colormap,SDist,std
+    half = int((len(percentiles)-1)/2)
+    
+    return half,percentiles,colormap,SDist
 
-###############################################################################
+# ###############################################################################
 damages = pd.read_csv('Results/SNP_damage.csv')
 scenarios = ['all_tax' , 'CO2', 'no_tax', 'SNP']
 scenarios_label = ['All' , 'CO2' , 'No', 'SNP']
-
 
 
 for sen in scenarios:
     
     hist = np.array(damages[sen]).reshape((500,365))
     
-    half,percentiles,colormap,SDist,std=process(hist)
+    half,percentiles,colormap,SDist = process(hist)
     
     # plt.rcParams['font.weight']='bold'
     plt.figure(figsize=(20,5))
@@ -57,15 +52,17 @@ for sen in scenarios:
     ax1.set_facecolor('white')
     plt.style.use('seaborn-paper')
     ax1.plot(np.arange(0,365,1), SDist[:,half],color='black',label='Median')
+   
     for i in range(len(percentiles)-1):
     
         if i <5:
-            index= str(percentiles[i]) + ' percentiles'
+            index= str(percentiles[i]) + 'th percentile'
         else:
-            index= str(percentiles[i+1]) + ' percentiles'
+            index= str(percentiles[i+1]) + 'th percentile'
         ax1.fill_between(np.arange(0,365,1), SDist[:,i],SDist[:,i+1],color=colormap(i/len(percentiles)),label= index)
     
-    ax1.set_xlabel('Day of the year', fontsize=12,fontweight='bold')
+    ax1.set_xlabel('Day of the year', fontsize=14,fontweight='bold')
+    ax1.set_ylabel('Damages ($)',fontsize=14,fontweight='bold')
     ax1.set_visible(True)
     ax1.legend(loc='right', bbox_to_anchor=(1.25, 0.5),ncol=1, fancybox=True, shadow=True,fontsize='medium')
     plt.style.use('seaborn-paper')
